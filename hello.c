a4c8f47bb6696ed9f3ce8be333bb132a9a5b69a5
Adam Sweeney <asweeney7@huskers.unl.edu>
2025-08-28 17:52:08 -0500

    Added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Adam Sweeney
+ *         asweeney7@huskers.unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Adam Sweeney!\n");
+  printf("Computer Science!\n");
+
+
+  return 0;
+}
